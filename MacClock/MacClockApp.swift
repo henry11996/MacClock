c0d26caceb74975726473bb39d9613238106c1c4
Henry <henry.fuhung.wu@gmail.com>
2026-02-02 10:41:33 +0800

    Initial Commit

---
+++ MacClock/MacClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacClockApp.swift
+//  MacClock
+//
+//  Created by 吳阜紘 on 2026/2/2.
+//
+
+import SwiftUI
+
+@main
+struct MacClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
